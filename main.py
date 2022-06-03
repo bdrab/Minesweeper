262dfe03fb39deefcc37931e6e69cbd241b2b52e
bdrab <bartekdrab4@wp.pl>
2022-06-03 11:37:13 +0200

    flag counter and timer added

--- main.py
+++ main.py
@@ -5,24 +5,31 @@ from tkinter import messagebox
 
 Tk().wm_withdraw()
 
-CELL_SIZE = 60
+CELL_SIZE = 20
 WIDTH = CELL_SIZE
 HEIGHT = CELL_SIZE
 MARGIN = 5
 BLACK = (0, 0, 0)
 GREY = (120, 120, 120)
 WHITE = (255, 255, 255)
+BUTTON_HEIGHT = 2 * CELL_SIZE
 
 
-board_size = 10
-bomb_number = 10
+board_size = 12
+bomb_number = 20
+time = 0
+flag_counter = bomb_number
 game_is_end = False
 game_result = ""
-window_size = [CELL_SIZE*board_size + (board_size+1)*MARGIN, CELL_SIZE*board_size + (board_size+1)*MARGIN]
+WIN_WIDTH = CELL_SIZE * board_size + (board_size+1) * MARGIN
+WIN_HEIGHT = CELL_SIZE * board_size + (board_size+1) * MARGIN + BUTTON_HEIGHT
+window_size = [WIN_WIDTH, WIN_HEIGHT]
 
 pygame.init()
 clock = pygame.time.Clock()
 scr = pygame.display.set_mode(window_size)
+font = pygame.font.Font(None, CELL_SIZE + 5)
+pygame.time.set_timer(pygame.USEREVENT, 1000)
 
 pygame.display.set_caption("Grid")
 
@@ -67,57 +74,91 @@ number_pictures = {
     7: picture_7,
     8: picture_8
 }
+scr.fill(BLACK)
+
+
+def bomb_counter():
+    bomb_counter_width = (board_size // 2) * CELL_SIZE + (board_size // 2 - 1) * MARGIN
+    bomb_counter_rect = pygame.draw.rect(scr, WHITE, [MARGIN, MARGIN, bomb_counter_width, BUTTON_HEIGHT - MARGIN])
+    text_bomb_counter = font.render(f"{flag_counter}", True, BLACK)
+    text_rect_bomb_counter = text_bomb_counter.get_rect(center=(bomb_counter_rect.centerx, bomb_counter_rect.centery))
+    scr.blit(text_bomb_counter, text_rect_bomb_counter)
+    pygame.display.flip()
+
+
+def timer(time_value):
+
+    timer_width = (board_size // 2) * CELL_SIZE + (board_size // 2 - 1) * MARGIN
+    timer_rect = pygame.draw.rect(scr, WHITE, [WIN_WIDTH - MARGIN - timer_width, MARGIN, timer_width, BUTTON_HEIGHT - MARGIN])
+    text_timer = font.render(f"{time_value}", True, BLACK)
+    text_rect_timer = text_timer.get_rect(center=(timer_rect.centerx, timer_rect.centery))
+    scr.blit(text_timer, text_rect_timer)
+    pygame.display.flip()
+    return time_value + 1
+
+
+bomb_counter()
+timer(0)
+
 
 while True:
+
     minesweeper = Minesweeper(board_size=board_size, bombs_number=bomb_number)
     while not game_is_end:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 game_is_end = True
+            elif event.type == pygame.USEREVENT:
+                time = timer(time)
             elif event.type == pygame.MOUSEBUTTONDOWN:
                 if event.button == 3:
                     pos = pygame.mouse.get_pos()
                     column = pos[0] // (WIDTH + MARGIN)
-                    row = pos[1] // (HEIGHT + MARGIN)
-                    if minesweeper.board[row][column] == "flag":
-                        minesweeper.board[row][column] = "A"
-                    elif minesweeper.board[row][column] == "A":
-                        minesweeper.board[row][column] = "flag"
+                    row = (pos[1] - BUTTON_HEIGHT) // (HEIGHT + MARGIN)
+                    if row >= 0:
+                        if minesweeper.board[row][column] == "flag":
+                            minesweeper.board[row][column] = "A"
+                            flag_counter += 1
+                            bomb_counter()
+                        elif minesweeper.board[row][column] == "A":
+                            flag_counter -= 1
+                            minesweeper.board[row][column] = "flag"
+                            bomb_counter()
 
                 if event.button == 1:
                     pos = pygame.mouse.get_pos()
                     column = pos[0] // (WIDTH + MARGIN)
-                    row = pos[1] // (HEIGHT + MARGIN)
-                    if minesweeper.board[row][column] != "flag":
-                        if [row, column] in minesweeper.bombs:
-                            game_is_end = True
-                            game_result = "LOSE"
-                            for bomb in minesweeper.bombs:
-                                minesweeper.board[bomb[0]][bomb[1]] = "bomb"
-                        elif minesweeper.scores[row][column] != 0:
-                            minesweeper.board[row][column] = minesweeper.scores[row][column]
-                        elif minesweeper.scores[row][column] == 0 and minesweeper.board[row][column] != 0:
-                            data = minesweeper.check_to_show([row, column])
-                            for cell in data:
-                                minesweeper.board[cell[0]][cell[1]] = minesweeper.scores[cell[0]][cell[1]]
+                    row = (pos[1] - BUTTON_HEIGHT) // (HEIGHT + MARGIN)
+                    if row >= 0:
+                        if minesweeper.board[row][column] != "flag":
+                            if [row, column] in minesweeper.bombs:
+                                game_is_end = True
+                                game_result = "LOSE"
+                                for bomb in minesweeper.bombs:
+                                    minesweeper.board[bomb[0]][bomb[1]] = "bomb"
+                            elif minesweeper.scores[row][column] != 0:
+                                minesweeper.board[row][column] = minesweeper.scores[row][column]
+                            elif minesweeper.scores[row][column] == 0 and minesweeper.board[row][column] != 0:
+                                data = minesweeper.check_to_show([row, column])
+                                for cell in data:
+                                    minesweeper.board[cell[0]][cell[1]] = minesweeper.scores[cell[0]][cell[1]]
 
-        scr.fill(BLACK)
         for row in range(minesweeper.board_size):
             for column in range(minesweeper.board_size):
                 if minesweeper.board[row][column] == "flag":
-                    scr.blit(picture_flag, ((MARGIN + WIDTH) * column + MARGIN, (MARGIN + HEIGHT) * row + MARGIN))
+                    scr.blit(picture_flag, ((MARGIN + WIDTH) * column + MARGIN, (MARGIN + HEIGHT) * row + MARGIN + BUTTON_HEIGHT))
                 elif minesweeper.board[row][column] == "bomb":
-                    scr.blit(picture_bomb, ((MARGIN + WIDTH) * column + MARGIN, (MARGIN + HEIGHT) * row + MARGIN))
+                    scr.blit(picture_bomb, ((MARGIN + WIDTH) * column + MARGIN, (MARGIN + HEIGHT) * row + MARGIN + BUTTON_HEIGHT))
                 elif minesweeper.board[row][column] == 0:
-                    pygame.draw.rect(scr, WHITE, [(MARGIN + WIDTH) * column + MARGIN, (MARGIN + HEIGHT) * row + MARGIN, WIDTH, HEIGHT])
+                    pygame.draw.rect(scr, WHITE, [(MARGIN + WIDTH) * column + MARGIN, (MARGIN + HEIGHT) * row + MARGIN + BUTTON_HEIGHT, WIDTH, HEIGHT])
 
                 elif minesweeper.board[row][column] == "A":
                     pygame.draw.rect(scr, GREY,
-                                     [(MARGIN + WIDTH) * column + MARGIN, (MARGIN + HEIGHT) * row + MARGIN, WIDTH, HEIGHT])
+                                     [(MARGIN + WIDTH) * column + MARGIN, (MARGIN + HEIGHT) * row + MARGIN + BUTTON_HEIGHT, WIDTH, HEIGHT])
 
                 else:
                     value = minesweeper.board[row][column]
-                    scr.blit(number_pictures[value], ((MARGIN + WIDTH) * column + MARGIN, (MARGIN + HEIGHT) * row + MARGIN))
+                    scr.blit(number_pictures[value], ((MARGIN + WIDTH) * column + MARGIN, (MARGIN + HEIGHT) * row + MARGIN + BUTTON_HEIGHT))
 
         win_list = []
         win = True
@@ -146,6 +187,9 @@ while True:
     if user_choice:
         game_is_end = False
         game_result = ""
+        time = 0
+        flag_counter = bomb_number
+        bomb_counter()
     else:
         game_is_end = False
         pygame.quit()
